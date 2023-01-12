4b39ede04d4025a7bcfa579eac68e2db9d77fc79
Ishan <ishansharmahrms@gmail.com>
2023-01-11 20:00:48 -0500

    Activity 1.1 is done.

---
+++ Activity 1.1 - Introduction to Python/LeapYear.py
@@ -0,0 +1,13 @@
+while True:
+    year = int(input("Enter a year: "))
+
+    if year < 999 or year > 9999:
+        print ("You entered an invalid year.")
+        continue
+
+    if year % 4 == 0 and year % 100 != 0:
+        print (year,"is a leap year.")
+    elif year % 400 == 0: 
+        print (year,"is a leap year.")
+    else:
+        print(year, "is not a leap year.")
